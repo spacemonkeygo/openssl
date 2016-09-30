fd0849ed037322acc4a32d619de9e11eb035bbb9
Jeff Wendling <jeff@spacemonkey.com>
2016-09-30 11:08:13 -0600

    add flags to work with homebrew openssl by default
    Change-Id: I1599b4585850d81d4bc43dc067321af15ad3fafd

--- build.go
+++ build.go
@@ -18,5 +18,6 @@ package openssl
 
 // #cgo pkg-config: libssl libcrypto
 // #cgo windows CFLAGS: -DWIN32_LEAN_AND_MEAN
-// #cgo darwin CFLAGS: -Wno-deprecated-declarations
+// #cgo darwin CFLAGS: -Wno-deprecated-declarations -I/usr/local/opt/openssl/include
+// #cgo darwin LDFLAGS: -L/usr/local/opt/openssl/lib
 import "C"
