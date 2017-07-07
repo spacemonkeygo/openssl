ad5fdecfce7d1ff493ec69d7e5fa05e26089a112
Jeff <leterip@me.com>
2017-07-07 14:49:22 -0600

    ignore linker warnings on os x
    this way if you don't have openssl@1.1 installed, it doesn't complain at you

--- build.go
+++ build.go
@@ -17,6 +17,6 @@ package openssl
 // #cgo pkg-config: libssl libcrypto
 // #cgo linux CFLAGS: -Wno-deprecated-declarations
 // #cgo darwin CFLAGS: -I/usr/local/opt/openssl@1.1/include -I/usr/local/opt/openssl/include -Wno-deprecated-declarations
-// #cgo darwin LDFLAGS: -L/usr/local/opt/openssl@1.1/lib -L/usr/local/opt/openssl/lib
+// #cgo darwin LDFLAGS: -w -L/usr/local/opt/openssl@1.1/lib -L/usr/local/opt/openssl/lib
 // #cgo windows CFLAGS: -DWIN32_LEAN_AND_MEAN
 import "C"
