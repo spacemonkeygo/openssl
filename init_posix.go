3d5df67ba7ee392569fadc2307d4c00dc8169470
Steven Allen <steven@stebalien.com>
2019-08-01 16:40:57 -0700

    feat: openbsd support

--- init_posix.go
+++ init_posix.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build linux darwin solaris freebsd
+// +build linux darwin solaris freebsd openbsd
 // +build !windows
 
 package openssl
