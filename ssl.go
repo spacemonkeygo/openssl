b724221eb6202430b861fff940d8f69b601455a5
Peter Grzybowski <peter@northern.tech>
2020-08-02 05:47:00 +0200

    Added security level mutator methods.
    New funcs:
    
    * GetSecurityLevel() int
      * mapped to SSL_get_security_level
      * https://www.openssl.org/docs/ssl/SSL_get_security_level.html
    
    * SetSecurityLevel(level int)
      * mapped to SSL_set_security_level
      * https://www.openssl.org/docs/ssl/SSL_set_security_level.html
    
    Signed-off-by: Peter Grzybowski <peter@northern.tech>

--- ssl.go
+++ ssl.go
@@ -72,6 +72,18 @@ func (s *SSL) GetServername() string {
 	return C.GoString(C.SSL_get_servername(s.ssl, C.TLSEXT_NAMETYPE_host_name))
 }
 
+// GetSecurityLevel gets the SSL security level. See
+// https://www.openssl.org/docs/ssl/SSL_get_security_level.html
+func (s *SSL) GetSecurityLevel() int {
+	return int(C.X_SSL_get_security_level(s.ssl))
+}
+
+// SetSecurityLevel sets the SSL security level. See
+// https://www.openssl.org/docs/ssl/SSL_set_security_level.html
+func (s *SSL) SetSecurityLevel(level int) {
+	C.X_SSL_set_security_level(s.ssl, C.int(level))
+}
+
 // GetOptions returns SSL options. See
 // https://www.openssl.org/docs/ssl/SSL_CTX_set_options.html
 func (s *SSL) GetOptions() Options {
